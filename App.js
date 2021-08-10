0a652c0bff7e7d68e63661d87579a9b6d5b676c7
noSpaghettiCode <btzemil@gmail.com>
2021-08-10 22:28:46 +0300

    Added Onboarding

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import OnBoardingScreen from './screens/OnBoardingScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -9,25 +10,39 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 const AppStack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator
-        headerMode="none"
-      >
-        <AppStack.Screen name="OnBoarding" component={OnBoardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch == null) {
+    return null;
+  } else if (isFirstLaunch == true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <AppStack.Screen name="OnBoarding" component={OnBoardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <LoginScreen/>
+  }
 }
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/OnBoardingScreen.js
+++ screens/OnBoardingScreen.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, StatusBar, TouchableOpacity } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper'
+
+const Dots = ({selected}) => {
+  let backgroundColor;
+
+  backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+  return (
+    <View
+      style={{
+        width: 5,
+        height: 5,
+        marginHorizontal: 3,
+        backgroundColor
+      }}
+    />
+  );
+}
+
+const Skip = ({...props}) => (
+  <TouchableOpacity {...props}>
+    <Text style={styles.skip}>
+      Skip
+    </Text>
+  </TouchableOpacity>
+)
+
+const Next = ({...props}) => (
+  <TouchableOpacity {...props}>
+    <Text style={styles.next}>
+      Next
+    </Text>
+  </TouchableOpacity>
+)
+
+const Done = ({...props}) => (
+  <TouchableOpacity {...props}>
+    <Text style={styles.next}>
+      Done
+    </Text>
+  </TouchableOpacity>
+)
 
 const OnBoardingScreen = ({navigation}) => {
     return (
-        <View style={styles.container}>
-            <Text>Onboarding screen</Text>
-                <Button
-                    title="Click here"
-                    onPress={() => navigation.navigate("Login")}
-                />
-        </View>
+      <View style={styles.container}>
+
+        <StatusBar translucent backgroundColor="transparent" />
+
+        <Onboarding 
+          SkipButtonComponent={Skip}
+          NextButtonComponent={Next}
+          DoneButtonComponent={Done}
+          DotComponent={Dots}
+          onSkip={() => navigation.replace("Login")}
+          onDone={() => navigation.navigate("Login")}
+          pages={[
+          {
+            backgroundColor: '#a6e4d0',
+            image: <Image source={require('../assets/onboarding-img1.png')} />,
+            title: 'Connect to the World',
+            subtitle: 'A New Way to Connect With The World',
+          },
+          {
+            backgroundColor: '#fdeb93',
+            image: <Image source={require('../assets/onboarding-img2.png')} />,
+            title: 'Share Your Favorites',
+            subtitle: 'Share Your Thoughts With Similar Kind of People',
+          },
+          {
+            backgroundColor: '#e9bcbe',
+            image: <Image source={require('../assets/onboarding-img3.png')} />,
+            title: 'Become The Star',
+            subtitle: 'Let The Spot Light Capture You',
+          },
+        ]}
+      />
+    </View>
     )
 }
 
@@ -18,7 +86,15 @@ export default OnBoardingScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white'
   },
+  skip: {
+    marginLeft: 20,
+    fontSize: 18,
+  },
+  next: {
+    marginRight: 20,
+    fontSize: 18
+  },
+ 
 });
