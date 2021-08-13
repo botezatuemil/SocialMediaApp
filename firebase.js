eb813ef63f1144c5b11778a6a313d69f66719ed7
noSpaghettiCode <btzemil@gmail.com>
2021-08-13 11:06:18 +0300

    Firebase auth final

--- firebase.js
+++ firebase.js
@@ -2,15 +2,7 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCfClvEB99PHfpBMR1ljH71xCFg4Du6Kak",
-    authDomain: "rn-social-app-3d06f.firebaseapp.com",
-    projectId: "rn-social-app-3d06f",
-    storageBucket: "rn-social-app-3d06f.appspot.com",
-    messagingSenderId: "331347660742",
-    appId: "1:331347660742:web:9d552cf8d8df0915cb2cc7",
-    measurementId: "G-5KC1NKP8TM"
-  };
+// 
 
 let app;
 
