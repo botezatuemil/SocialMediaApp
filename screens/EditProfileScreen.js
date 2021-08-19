fa024608ec1d034d376dad1da3a7b3d92749e1b9
noSpaghettiCode <btzemil@gmail.com>
2021-08-19 23:09:33 +0300

    Uploaded profile picture in edit and change name

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,12 +1,290 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, ImageBackground, TextInput, KeyboardAvoidingView } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import * as ImagePicker from 'expo-image-picker';
+
+import FormButton from '../components/FormButton';
 
 const EditProfileScreen = ({navigation}) => {
-    return (
-        <View>
 
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("Permission to access camera roll is required!");
+          return;
+        }
+    
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        console.log(pickerResult);
+    
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+    
+        setSelectedImage({ localUri: pickerResult.uri });
+    } 
+
+    const renderInner = () => (
+        <View style={styles.panel}>
+            <View style={{alignItems: 'center'}}>
+                <Text style={styles.panelTitle}>Upload photo</Text>
+                <Text style={styles.panelSubtitle}>Choose your profile picture</Text>
+            </View>
+
+            <TouchableOpacity style={styles.panelButton}>
+                <Text style={styles.panelButtonTitle}>Take Photo</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.panelButton} onPress={openImagePickerAsync} >
+                <Text style={styles.panelButtonTitle}>Choose from Library</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.panelButton} onPress={() => bs.current.snapTo(1)}>
+                <Text style={styles.panelButtonTitle}>Cancel</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+    const renderHeader = () => (
+        <View style={styles.header}>
+            <View style={styles.panelHeader}>
+                <View style={styles.panelHandle}>
+                </View>
+            </View>
+        </View>
+    )
+    
+    let bs = React.createRef();
+    let fall = new Animated.Value(1);
+
+    return (
+        <View style={styles.container}>
+            <BottomSheet
+                ref={bs}
+                snapPoints={[330, 0]}
+                renderContent={renderInner}
+                renderHeader={renderHeader}
+                initialSnap={1}
+                callbackNode={fall}
+                enabledGestureInteraction={true}
+                enabledContentTapInteraction={false} 
+            />
+            <Animated.View style={{margin: 20, opacity: Animated.add(0.3, Animated.multiply(fall, 1.0))}}>
+                <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity  onPress={() => bs.current.snapTo(0)}>
+                        <View style={{
+                            height: 100,
+                            width: 100,
+                            borderRadius: 100,
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                            
+                            
+                            <ImageBackground
+                                source={ selectedImage != null ? { uri : selectedImage.localUri} : require('../assets/users/user-8.jpg')}
+                                style={{height: 100, width: 100,}}
+                                imageStyle={{borderRadius: 15}}
+                            >
+                                <View
+                                    style={{
+                                        flex: 1,
+                                        justifyContent: 'center',
+                                        alignItems: 'center'
+                                    }}
+                                >
+                                    <Icon name="camera" size={35} color="#fff" style={{
+                                        opacity: 0.7,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderWidth: 1,
+                                        borderColor: '#fff',
+                                        borderRadius: 10,
+                                    }}
+                                    />
+                                </View>
+                            </ImageBackground>
+                        </View>
+                    </TouchableOpacity>
+
+                    <Text style={{marginTop: 10, fontSize: 20, fontWeight: 'bold'}}>{name} {lastName}</Text>
+                </View>
+
+                <View style={styles.action}>
+                    <FontAwesome name="user-o" size={20} />
+                    <TextInput
+                        placeholder="First name"
+                        placeholderTextColor="#666666"
+                        autoCorect={false}
+                        style={styles.textInput}
+                        onChangeText={(txt) => setName(txt)}
+                    />
+                </View>
+
+                <View style={styles.action}>
+                    <FontAwesome name="user-o" size={20} />
+                    <TextInput
+                        placeholder="Last name"
+                        placeholderTextColor="#666666"
+                        autoCorect={false}
+                        style={styles.textInput}
+                        onChangeText={(txt) => setLastName(txt)}
+                    />
+                </View>
+
+                <View style={styles.action}>
+                    <Feather name="phone" size={20} />
+                    <TextInput
+                        placeholder="Phone"
+                        placeholderTextColor="#666666"
+                        keyboardType="number-pad"
+                        autoCorect={false}
+                        style={styles.textInput}
+                    />
+                </View>
+
+                <View style={styles.action}>
+                    <FontAwesome name="envelope-o" size={20} />
+                    <TextInput
+                        placeholder="Email"
+                        placeholderTextColor="#666666"
+                        keyboardType="email-address"
+                        autoCorect={false}
+                        style={styles.textInput}
+                    />
+                </View>
+
+                <View style={styles.action}>
+                    <FontAwesome name="globe" size={20} />
+                    <TextInput
+                        placeholder="Country"
+                        placeholderTextColor="#666666"
+                        autoCorect={false}
+                        style={styles.textInput}
+                    />
+                </View>
+
+                <KeyboardAwareScrollView style={styles.action}>
+                    <Icon name="map-marker-outline" size={20} />
+                    <TextInput
+                        placeholder="City"
+                        placeholderTextColor="#666666"
+                        autoCorect={false}
+                        style={styles.textInput, {marginTop: -25, marginLeft: 28}}
+                    />
+                </KeyboardAwareScrollView >
+
+                {/* <TouchableOpacity style={styles.commandButton} }>
+                    <Text style={styles.panelButtonTitle}>Submit</Text>
+                </TouchableOpacity> */}
+                <FormButton buttonTitle="Update"/>
+
+            </Animated.View>
         </View>
     )
 }
 
 export default EditProfileScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+    commandButton: {
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: '#FF6347',
+        alignItems: 'center',
+        marginTop: 10,
+        width: 350,
+        alignSelf: 'center',
+    },
+    panel: {
+        padding: 20,
+        backgroundColor: '#FFFFFF',
+        paddingTop: 20,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+    },
+    header: {
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#333333',
+        shadowOffset: {width: -1, height: -3},
+        shadowRadius: 2,
+        shadowOpacity: 0.4,
+    // elevation: 5,
+    paddingTop: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    panelHeader: {
+        alignItems: 'center',
+    },
+    panelHandle: {
+        width: 40,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: '#00000040',
+        marginBottom: 10,
+    },
+    panelTitle: {
+        fontSize: 24,
+        height: 35,
+    },
+    panelSubtitle: {
+        fontSize: 14,
+        color: 'gray',
+        height: 30,
+        marginBottom: 10,
+    },
+    panelButton: {
+        padding: 13,
+        borderRadius: 10,
+        backgroundColor: '#2e64e5',
+        alignItems: 'center',
+        marginVertical: 7,
+    },
+    panelButtonTitle: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5,
+    },
+    actionError: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#FF0000',
+        paddingBottom: 5,
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -4,
+        paddingLeft: 10,
+        color: '#05375a',
+    },
+});
